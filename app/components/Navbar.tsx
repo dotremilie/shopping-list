f77c05b76f2d1a087f9ef822c58c1f568377e7a3
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-06-09 17:42:44 +0200

    Add Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import { FiShoppingBag } from 'react-icons/fi'
+import { usePathname } from 'next/navigation'
+import { clsx } from 'clsx'
+
+const Navbar = () => {
+    const pathname = usePathname()
+
+    return (
+        <div className="text-2xl absolute flex items-center justify-around bottom-0 left-0 w-full dark:bg-stone-900 bg-stone-200">
+            <Link
+                href="/"
+                className={clsx(
+                    'gap-2 h-full p-4 flex flex-col items-center justify-center',
+                    {
+                        'text-blue-500': pathname === '/',
+                    },
+                )}
+            >
+                <FiShoppingBag />
+            </Link>
+        </div>
+    )
+}
+
+export default Navbar
