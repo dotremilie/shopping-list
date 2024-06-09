ccb726f6dd5db582cd2c717fea68774ba4489361
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-06-09 23:17:21 +0200

    remove by ID and by multiple IDs

--- _actions/removeItems.ts
+++ _actions/removeItems.ts
@@ -4,16 +4,16 @@ import ItemModel from '@/models/ItemModel'
 import connectDB from '@/config/database'
 import Item from '@/models/Item'
 
-export async function removeItem(id: string) {
+export async function removeItems(ids: string[]) {
     try {
         await connectDB()
 
-        const removedItem = await ItemModel.findByIdAndDelete(id)
+        const removedItem = await ItemModel.deleteMany({_id: {$in: ids}})
 
         if (removedItem) {
             console.log('Item removed:', removedItem)
         } else {
-            console.log('Item not found with id:', id)
+            console.log('Item not found with id in ', ids)
         }
     } catch (err) {
         console.error('Error removing item:', err)
